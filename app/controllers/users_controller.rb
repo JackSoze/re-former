9bd82e27ef4e4f32045bfbe6d03baa4522aae0cf
jacksoze <kamonyojack@gmail.com>
2023-07-15 17:36:01 +0300

    Add all other restful routes with ass controllers actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :exception
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -13,6 +18,26 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_params)
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def allowed_params
     params.require(:user).permit(:username, :email, :password)
