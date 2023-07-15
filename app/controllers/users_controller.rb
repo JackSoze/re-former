a31e6b0e354e77234e191a3636cd36221a2caef1
jacksoze <kamonyojack@gmail.com>
2023-07-15 11:39:54 +0300

    Create form with form_with and style with bootstrap
    Validations errors also shown on forms on errors
    Changed bootstrap is-valid(not working) to text-danger

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
-  def new; end
+  protect_from_forgery with: :exception
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new(allowed_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def allowed_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
